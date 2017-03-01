c7fefd7c8260071af581076f48cfa099ab8eeb6c
luxingyu <luxingyu@mockuai.com>
2017-03-01 17:22:49 +0800

    角色管理

---
+++ src/fn/role-management.js
@@ -0,0 +1,24 @@
+/**
+ * Created by kyn on 17/3/1.
+ */
+;(function(){
+    var main={
+        init:function(){
+            var that=this;
+            that.Verification();
+        },
+        Verification:function(){
+            var validator = new FormValidator();
+            validator.settings.alerts = true;
+            $('.btn-search').click(function () {
+                for (var i = 0; i < $('[required]').length; i++) {
+                    var required = $('[required]');
+                    validator.checkField.call(validator, required.eq(i))
+                }
+            });
+        }
+    };
+    $(function(){
+        main.init()
+    })
+})();
