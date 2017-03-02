a3fb3e12cc6da1d37862ecf1eeb9d192cdff2dc9
luxingyu <luxingyu@mockuai.com>
2017-03-02 17:52:57 +0800

    管控平台新页面

--- src/fn/role-management.js
+++ src/fn/role-management.js
@@ -4,19 +4,8 @@
 ;(function(){
     var main={
         init:function(){
-            var that=this;
-            that.Verification();
-        },
-        //Verification:function(){
-        //    var validator = new FormValidator();
-        //    validator.settings.alerts = true;
-        //    $('.btn-search').click(function () {
-        //        for (var i = 0; i < $('[required]').length; i++) {
-        //            var required = $('[required]');
-        //            validator.checkField.call(validator, required.eq(i))
-        //        }
-        //    });
-        //}
+
+        }
     };
     $(function(){
         main.init()
