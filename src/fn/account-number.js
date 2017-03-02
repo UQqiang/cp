720ddd8124c413c42beba4282e29ee791c4616f9
luxingyu <luxingyu@mockuai.com>
2017-03-02 09:59:17 +0800

    页面修改

--- src/fn/account-number.js
+++ src/fn/account-number.js
@@ -5,18 +5,18 @@
     var main={
         init:function(){
             var that=this;
-            that.Verification();
+            //that.Verification();
         },
-        Verification:function(){
-            var validator = new FormValidator();
-            validator.settings.alerts = true;
-            $('.btn-search').click(function () {
-                for (var i = 0; i < $('[required]').length; i++) {
-                    var required = $('[required]');
-                    validator.checkField.call(validator, required.eq(i))
-                }
-            });
-        }
+        //Verification:function(){
+        //    var validator = new FormValidator();
+        //    validator.settings.alerts = true;
+        //    $('.btn-search').click(function () {
+        //        for (var i = 0; i < $('[required]').length; i++) {
+        //            var required = $('[required]');
+        //            validator.checkField.call(validator, required.eq(i))
+        //        }
+        //    });
+        //}
     };
     $(function(){
         main.init()
--- src/fn/add-account.js
+++ src/fn/add-account.js
@@ -5,19 +5,19 @@
     var main={
         init:function(){
             var that=this;
-            that.Verification();
+            //that.Verification();
 
         },
-        Verification:function(){
-            var validator = new FormValidator();
-            validator.settings.alerts = true;
-            $('.btn-save').click(function () {
-                for (var i = 0; i < $('[required]').length; i++) {
-                    var required = $('[required]');
-                    validator.checkField.call(validator, required.eq(i))
-                }
-            });
-        }
+        //Verification:function(){
+        //    var validator = new FormValidator();
+        //    validator.settings.alerts = true;
+        //    $('.btn-save').click(function () {
+        //        for (var i = 0; i < $('[required]').length; i++) {
+        //            var required = $('[required]');
+        //            validator.checkField.call(validator, required.eq(i))
+        //        }
+        //    });
+        //}
     };
     $(function(){
         main.init();
