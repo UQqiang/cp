b3e2df451fb0582150cbc56c8bab2bec75c085f4
luxingyu <luxingyu@mockuai.com>
2017-03-02 10:59:06 +0800

    添加角色页面增加

--- src/fn/role-management.js
+++ src/fn/role-management.js
@@ -7,16 +7,16 @@
             var that=this;
             that.Verification();
         },
-        Verification:function(){
-            var validator = new FormValidator();
-            validator.settings.alerts = true;
-            $('.btn-search').click(function () {
-                for (var i = 0; i < $('[required]').length; i++) {
-                    var required = $('[required]');
-                    validator.checkField.call(validator, required.eq(i))
-                }
-            });
-        }
+        //Verification:function(){
+        //    var validator = new FormValidator();
+        //    validator.settings.alerts = true;
+        //    $('.btn-search').click(function () {
+        //        for (var i = 0; i < $('[required]').length; i++) {
+        //            var required = $('[required]');
+        //            validator.checkField.call(validator, required.eq(i))
+        //        }
+        //    });
+        //}
     };
     $(function(){
         main.init()
