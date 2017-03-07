bf2a00c5b7e6f77b824b8bd91910524342fc03bd
luxingyu <luxingyu@mockuai.com>
2017-03-07 18:28:23 +0800

    运费模板页面

--- src/fn/add-account.js
+++ src/fn/add-account.js
@@ -6,17 +6,45 @@
         init:function(){
             var that=this;
             that.Verification();
-
+            that.addEvent();
         },
+        //验证
         Verification:function(){
+            var that=this;
             var validator = new FormValidator();
             validator.settings.alerts = true;
             $('.btn-save').click(function () {
+                var isValid = true;
                 for (var i = 0; i < $('[required]').length; i++) {
                     var required = $('[required]');
-                    validator.checkField.call(validator, required.eq(i))
+                    var result = validator.checkField.call(validator, required.eq(i));
+                    if (result.valid === false) {
+                        isValid = false;
+                    }
+                }
+                if( isValid == true ){
+                    that.acquisition();
                 }
             });
+        },
+        acquisition:function(){
+            var data={};
+            data.name = $("input[cb-node='name']").val();
+            data.role = $("select[cb-node='role_id']").val();
+            data.user = $("input[cb-node='user_name']").val();
+            data.password = $("input[cb-node='password']").val();
+            data.department = $("input[cb-node='department']").val();
+            data.post = $("input[cb-node='post']").val();
+            data.phone = $("input[cb-node='phone']").val();
+            data.telephone= $("input[cb-node='telephone']").val();
+            console.log(data);
+        },
+        addEvent:function(){
+            var that=this;
+
+        },
+        submitData:function(){
+
         }
     };
     $(function(){
