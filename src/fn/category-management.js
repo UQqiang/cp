e110abd7ece6746964a22ecc557dd830f496adab
lijiahao <lijiahao@mockuai.com>
2017-03-14 11:58:28 +0800

    commit: 品类开发完成

--- src/fn/category-management.js
+++ src/fn/category-management.js
@@ -6,6 +6,7 @@
         init: function () {
             this.currentCateObj = {};                   // 当前选择的一二级类目对象
             this.categoryId = '';
+            this.imageUrl = '';
             this.api = Api.domain();                    // 接口请求的api
             // 初始化提示框
             toastr.options = ({
@@ -13,7 +14,7 @@
                 positionClass: "toast-top-center"
             });
             var height = window.innerHeight - 200;
-            $('.step').css({
+            $('.category-list').css({
                 'min-height': height + 'px'
             });
             this.addEvent();
@@ -32,35 +33,66 @@
                 multiple: false,
                 selectSuccess: function (data, target) {
                     that.$currentImgUploadBtn = target;
-                    //that.imgSelected(data);
-                    that.dialogTip.show();
+                    that.imgSelected(data);
                 },
                 uploadSuccess: function (data, target) {
                     that.$currentImgUploadBtn = target;
-                    //that.imgSelected(data[0].url);
-                    that.dialogTip.show();
+                    that.imgSelected(data[0].url);
                 }
             });
+        },
+        imgSelected: function (url) {
+            console.log(url);
+            this.imageUrl = url;
+            if( this.$currentImgUploadBtn.find('.icon-image').length > 0 ){
+                this.$currentImgUploadBtn.find('.icon-image').attr('src',url);
+            }else{
+                this.$currentImgUploadBtn.append('<img class="icon-image" src="'+url+'">')
+            }
         },
         addEvent: function () {
             var that = this;
 
             $('.categories').on('click', '.j-add-category', function () {
+                // remove 之前的tip
+                if( that.dialogTip ){
+                    that.dialogTip.close();
+                }
                 var type = $(this).attr('data-type');
                 var data = {};
+                var d = {};
                 if (type == 1) {
-                    data.content = '<div class="add-category-dialog"><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称"></div>'
+                    data.content = '<div class="add-category-dialog"><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称"></div>';
+                    data.closeOnBodyClick = true;
+                    d.parent_id = 0;
                 } else if (type == 2) {
                     if( !that.categoryId ){
                         toastr.error('请选择一级类目','提示');
                         return false;
                     }
-                    data.content = '<div class="add-category-dialog"><a class="imgUploadBtn"><i class="fa fa-plus"></i></a><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称"></div>'
+                    data.content = '<div class="add-category-dialog"><a class="imgUploadBtn"><i class="fa fa-plus"></i></a><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称"></div>';
+                    data.closeOnBodyClick = false;
+                    d.parent_id = that.categoryId;
                 }
                 data.target = $(this);
                 data.width = 400;
                 data.position = 'bottom';
                 that.tip(data, function (btn, dialog) {
+                    d.category_name = $.trim($('.j-category-name').val());
+                    if( that.imageUrl ){
+                        d.image_url = that.imageUrl;
+                    }
+                    that.addCategory(d, function (cbData) {
+                        if( cbData.code == 10000 ){
+                            toastr.success('添加分类成功','提示');
+                            that.queryCategory(type);
+                            that.imageUrl = '';
+                        }else{
+                            toastr.error(cbData.msg,'提示');
+                        }
+                    }, function (cbData) {
+                        toastr.error(cbData.msg,'提示');
+                    });
                     dialog.close();
                 }, function (btn, dialog) {
                     dialog.close();
@@ -70,16 +102,69 @@
             });
 
             $('.categories').on('click', '.j-edit-category', function () {
-                that.queryCategory();
+                // remove 之前的tip
+                if( that.dialogTip ){
+                    that.dialogTip.close();
+                }
+                var id = $(this).parents('li').attr('data-id');
+                var name = $(this).parents('li').attr('data-name');
+                var type = $(this).parents('li').attr('data-cate_level');
+                var img = $(this).parents('li').attr('data-img');
+                var data = {};
+                var d = {};
+                data.content = '确定要删除该类目:&nbsp;' + name + '吗?';
+                data.target = $(this);
+                d.category_id = id;
+                if (type == 1) {
+                    data.closeOnBodyClick = true;
+                    data.width = 250;
+                    data.content = '<div class="add-category-dialog"><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称" value="'+name+'"></div>';
+                    d.parent_id = 0;
+                } else if (type == 2) {
+                    data.closeOnBodyClick = false;
+                    data.width = 400;
+                    if( img ){
+                        that.imageUrl = img;
+                        data.content = '<div class="add-category-dialog"><a class="imgUploadBtn"><i class="fa fa-plus"></i><img class="icon-image" src="'+img+'"></a><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称" value="'+name+'"></div>';
+                    }else{
+                        data.content = '<div class="add-category-dialog"><a class="imgUploadBtn"><i class="fa fa-plus"></i></a><input class="form-control form-control-lg j-category-name" placeholder="请输入类目名称" value="'+name+'"></div>';
+                    }
+                    d.parent_id = that.categoryId;
+                }
+                that.tip(data, function (btn, dialog) {
+                    d.category_name = $.trim($('.j-category-name').val());
+                    if( that.imageUrl ){
+                        d.image_url = that.imageUrl;
+                    }
+                    that.updateCategory(d, function (cbData) {
+                        if( cbData.code == 10000 ){
+                            toastr.success('编辑分类:&nbsp;"'+name+'"&nbsp;为&nbsp;"'+d.category_name+'"&nbsp;成功','提示');
+                            that.queryCategory(type);
+                            that.imageUrl = '';
+                        }else{
+                            toastr.error(cbData.msg,'提示');
+                        }
+                    }, function (cbData) {
+                        toastr.error(cbData.msg,'提示');
+                    });
+                    dialog.close();
+                }, function (btn, dialog) {
+                    dialog.close();
+                });
             });
 
             $('.categories').on('click', '.j-delete-category', function () {
+                // remove 之前的tip
+                if( that.dialogTip ){
+                    that.dialogTip.close();
+                }
                 var id = $(this).parents('li').attr('data-id');
                 var name = $(this).parents('li').attr('data-name');
                 var cateLevel = $(this).parents('li').attr('data-cate_level');
                 var data = {};
                 data.content = '确定要删除该类目:&nbsp;' + name + '吗?';
                 data.target = $(this);
+                data.closeOnBodyClick = true;
                 that.tip(data, function (btn, dialog) {
                     that.deleteCategory(id, function (d) {
                         toastr.success('删除成功', '提示');
@@ -109,7 +194,7 @@
             }, {
                 width: data.width || 200,
                 closeable: false,
-                closeOnBodyClick: false,
+                closeOnBodyClick: data.closeOnBodyClick,
                 buttonAlign: 'center',
                 buttons: [{
                     type: 'highlight',
@@ -150,6 +235,10 @@
 
                         if (cateLevel == 1) {
                             $('#add-goods-category-2').html('');
+                        }
+
+                        if( that.categoryId ){
+                            $('#add-goods-category-1').find('li[data-id='+ that.categoryId +']').click();
                         }
 
                         // 类目的点击事件
@@ -238,6 +327,34 @@
             var that = this;
             $.ajax({
                 url: that.api + '/bossmanager/category/add.do',
+                type: 'get',
+                dataType: 'jsonp',
+                data: data,
+                beforeSend: function () {
+
+                },
+                success: function (data) {
+                    if (data.code == 10000) {
+                        success && success(data);
+                    } else {
+                        error && error(data);
+                    }
+                },
+                complete: function () {
+
+                },
+                error: function (data) {
+                    error && error(data);
+                }
+            });
+        },
+        /**
+         * 编辑
+         */
+        updateCategory: function (data, success, error) {
+            var that = this;
+            $.ajax({
+                url: that.api + '/bossmanager/category/update.do',
                 type: 'get',
                 dataType: 'jsonp',
                 data: data,
